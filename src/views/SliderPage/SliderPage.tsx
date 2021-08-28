a73606b95c02ec718c90df03f042ab9a42229da7
kschoi <kyusch0330@gmail.com>
2021-08-29 03:43:46 +0900

    [Fix] 메뉴 슬라이드 스타일 수정

--- src/views/SliderPage/SliderPage.tsx
+++ src/views/SliderPage/SliderPage.tsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Slider from "components/Sliders/BasicSlider/Slider";
 import VerticalSlider from "components/Sliders/VerticalSlider/VerticalSlider";
+import { ReactComponent as ArrowImg } from "assets/right-arrow.svg";
 import "./SliderPage.scss";
 const SliderPage = () => {
+  const [display, setDisplay] = useState(true);
   const [current, setCurrent] = useState(0);
   const paths = [
     ["basic slider", "/slider"],
@@ -12,7 +14,7 @@ const SliderPage = () => {
   const components = [Slider, VerticalSlider];
   return (
     <div className="sliderPageContainer">
-      <div className="sliderMenuContainer">
+      <div className={`sliderMenuContainer ${!display && "closed"}`}>
         <div className="sliderMenu">
           {paths.map((path, pIndex) => (
             <Link
@@ -25,6 +27,12 @@ const SliderPage = () => {
             </Link>
           ))}
         </div>
+        <button
+          className="toggleMenuBtn"
+          onClick={() => setDisplay((display) => !display)}
+        >
+          <ArrowImg width={20} height={20} />
+        </button>
       </div>
       {paths.map((path, pIndex) => (
         <Route exact path={path[1]} component={components[pIndex]} />
