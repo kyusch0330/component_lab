86b742fb8c57f9500723640f6da3f5f2963c583e
kschoi <kyusch0330@gmail.com>
2021-08-29 03:04:12 +0900

    [Fix] SliderPage Link Route 렌더링 방식 수정

--- src/views/SliderPage/SliderPage.tsx
+++ src/views/SliderPage/SliderPage.tsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Slider from "components/Sliders/BasicSlider/Slider";
 import VerticalSlider from "components/Sliders/VerticalSlider/VerticalSlider";
 import "./SliderPage.scss";
 const SliderPage = () => {
+  const [current, setCurrent] = useState(0);
+  const paths = [
+    ["basic slider", "/slider"],
+    ["vertical slider", "/slider/vertical"],
+  ];
+  const components = [Slider, VerticalSlider];
   return (
     <div className="sliderPageContainer">
       <div className="sliderMenuContainer">
         <div className="sliderMenu">
-          <Link to="/slider">basic slider</Link>
-          <Link to="/slider/vertical">vertical slider</Link>
+          {paths.map((path, pIndex) => (
+            <Link
+              key={pIndex}
+              onClick={() => setCurrent(pIndex)}
+              className={`menu ${current === pIndex && "selected"}`}
+              to={path[1]}
+            >
+              {path[0]}
+            </Link>
+          ))}
         </div>
       </div>
-      <Route exact path="/slider" component={Slider} />
-      <Route exact path="/slider/vertical" component={VerticalSlider} />
+      {paths.map((path, pIndex) => (
+        <Route exact path={path[1]} component={components[pIndex]} />
+      ))}
     </div>
   );
 };
