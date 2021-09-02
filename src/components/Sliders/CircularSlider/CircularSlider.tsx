13d16bb8a56d38ff66cc2ade1abb59a3be635fd3
kschoi <kyusch0330@gmail.com>
2021-09-03 06:56:11 +0900

    [Fix] Circular Slider 로직 수정

--- src/components/Sliders/CircularSlider/CircularSlider.tsx
+++ src/components/Sliders/CircularSlider/CircularSlider.tsx
@@ -14,30 +14,43 @@ const CircularSlider = () => {
     if (paused) return;
     const interval = setInterval(() => {
       setCurrent((current) => (current + 1) % colors.length);
-    }, 5000);
+    }, 3000);
     return function clean() {
       clearInterval(interval);
     };
   }, [paused]);
 
+  console.log("--- current:", current);
   return (
     <div className="circular_sliderContainer">
       <div className="sliderView">
         <div className="slider">
-          {colors.map((color, index) => (
-            <div
-              className={`slideCard ${color}`}
-              style={{
-                zIndex:
-                  colors.length +
-                  (index > current ? -1 * index : index - current),
-                top:
-                  index > current
-                    ? 20 + 20 * (index - current) + "px"
-                    : 20 + -20 * (index - current) + "px",
-              }}
-            />
-          ))}
+          {colors.map((color, index) => {
+            const len = colors.length;
+            const range = Math.floor(colors.length / 2);
+            const start = current - range;
+            const end = current + range;
+            const reIndex =
+              index < start
+                ? len + index
+                : index > end
+                ? 0 - (len - index)
+                : index;
+            const isBigger = reIndex >= current;
+            const diff = Math.abs(current - reIndex);
+            return (
+              <div
+                className={`slideCard ${color}`}
+                style={{
+                  zIndex: len - diff,
+                  top: isBigger
+                    ? `${60 + 30 * diff}px`
+                    : `${60 + -30 * diff}px`,
+                  width: `${100 - diff * 10}%`,
+                }}
+              />
+            );
+          })}
         </div>
         <SliderController
           cardLen={colors.length}
