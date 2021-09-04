132c37db0ab38a8309b99957b8b06393565804bf
kschoi <kyusch0330@gmail.com>
2021-09-05 04:27:19 +0900

    [Feat] 터치환경 Drag and Drop 구현

--- src/components/DragAndDrops/BasicDragAndDrop/DragAndDrop.tsx
+++ src/components/DragAndDrops/BasicDragAndDrop/DragAndDrop.tsx
@@ -6,16 +6,16 @@ import { ReactComponent as DotsImg } from "assets/dots.svg";
 const DragAndDrop = () => {
   const [infos, setInfos] = useState(
     Array.from(Array(6), (_, index) => ({
-      num: index,
+      num: index, //num은 id 역할
       order: index,
     }))
   );
   const [currentPos, setCurrentPos] = useState({ y: 0, x: 0 });
   const [selected, setSelected] = useState(-1);
-  const [target, setTarget] = useState(-1);
+  // const [target, setTarget] = useState(-1); // for touch device
   const [dragging, setDragging] = useState(false);
-  const changeElements = () => {
-    const sIndex = infos.findIndex((info) => info.num === selected);
+  const changeElements = (select: number, target: number) => {
+    const sIndex = infos.findIndex((info) => info.num === select);
     const sOrder = Number(infos[sIndex].order);
     const tOrder = Number(infos.find((info) => info.num === target)?.order);
     const isTargetBehind = tOrder > sOrder;
@@ -47,7 +47,11 @@ const DragAndDrop = () => {
       style={{ cursor: `${dragging ? "grabbing" : "initial"}` }}
       onMouseUp={() => {
         if (!dragging) return;
-        console.log("DRAGEND");
+        setSelected(-1);
+        setDragging(false);
+      }}
+      onTouchEnd={() => {
+        if (!dragging) return;
         setSelected(-1);
         setDragging(false);
       }}
@@ -55,22 +59,62 @@ const DragAndDrop = () => {
         if (!dragging) return;
         setCurrentPos({ y: e.clientY, x: e.clientX });
       }}
+      onTouchMove={(e) => {
+        if (!dragging) return;
+        setCurrentPos({ y: e.touches[0].clientY, x: e.touches[0].clientX });
+      }}
     >
+      {/* <div
+        style={{
+          padding: "5px",
+          background: "red",
+          position: "fixed",
+          top: currentPos.y,
+          left: currentPos.x,
+          zIndex: 2000,
+        }}
+      >
+        {target}
+      </div> */}
+      {/* <div style={{ position: "fixed", top: "10px", left: "10px" }}>
+        {target}
+      </div> */}
       {infos.map((info, index) => (
         <div
           className="boxContainer"
           style={{ order: info.order }}
-          onMouseEnter={() => {
+          // onMouseEnter={() => {
+          //   if (!dragging) return;
+          //   setTarget(info.num);
+          // }}
+          // onMouseLeave={() => {
+          //   if (!dragging) return;
+          //   setTarget(-1);
+          // }}
+          onMouseEnter={(e) => {
             if (!dragging) return;
-            setTarget(info.num);
-          }}
-          onMouseLeave={() => {
-            if (!dragging) return;
-            setTarget(-1);
+            changeElements(selected, info.num);
           }}
           onMouseUp={() => {
             if (!dragging) return;
-            changeElements();
+            changeElements(selected, info.num);
+            setDragging(false);
+          }}
+          onTouchMove={(e) => {
+            if (!dragging) return;
+            const el = document.elementFromPoint(
+              Number(e.touches[0].pageX),
+              Number(e.touches[0].pageY)
+            );
+            if (!el || typeof el.className !== "string") return;
+            const classNames = el.className.split("_");
+            if (classNames[0] !== "box") return; //***
+            const targetOrder = Number(window.getComputedStyle(el).order); //***
+            const targetInfo = infos.find((info) => info.order === targetOrder);
+            if (targetInfo) changeElements(selected, targetInfo.num);
+          }}
+          onTouchEnd={(e) => {
+            if (!dragging) return;
             setDragging(false);
           }}
         >
@@ -81,20 +125,38 @@ const DragAndDrop = () => {
               position: `${selected === info.num ? "fixed" : "relative"}`,
               top: `${selected === info.num ? currentPos.y : 0}px`,
               left: `${selected === info.num ? currentPos.x : 0}px`,
+              order: info.order, // 터치 동작시 order를 구하기 위함
             }}
           >
             <div
-              className="grabArea"
+              className="box_grabArea"
+              style={{
+                order: info.order, // 터치 동작시 order를 구하기 위함
+              }}
               onMouseDown={(e) => {
-                console.log("DRAG START");
                 setDragging(true);
                 setSelected(info.num);
                 setCurrentPos({ y: e.clientY, x: e.clientX });
               }}
+              onTouchStart={(e) => {
+                setDragging(true);
+                setSelected(info.num);
+                setCurrentPos({
+                  y: e.touches[0].clientY,
+                  x: e.touches[0].clientX,
+                });
+              }}
             >
               <DotsImg />
             </div>
-            <div className="numInfo">{info.num}</div>
+            <div
+              style={{
+                order: info.order, // 터치 동작시 order를 구하기 위함
+              }}
+              className="box_numInfo"
+            >
+              {info.num}
+            </div>
           </div>
         </div>
       ))}
