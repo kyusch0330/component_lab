552d1054748d32b13c7f736af1077361efa4925a
kschoi <kyusch0330@gmail.com>
2021-09-13 23:58:11 +0900

    [Feat] Music 플레이 시간 표시, 시간 이동, 반복 재생 구현

--- src/components/SoundComponents/MusicController/MusicController.tsx
+++ src/components/SoundComponents/MusicController/MusicController.tsx
@@ -7,6 +7,7 @@ import music_thejazzpiano from "../../../assets/thejazzpiano.mp3";
 
 import { ReactComponent as PlayImg } from "assets/play.svg";
 import { ReactComponent as PauseImg } from "assets/pause.svg";
+import { ReactComponent as RepeatImg } from "assets/repeat.svg";
 
 const MusicController = () => {
   const musics = [
@@ -21,21 +22,36 @@ const MusicController = () => {
   ];
   const [currentMusic, setCurrentMusic] = useState(musics[0]);
   const [playing, setPlaying] = useState(false);
+  const [loop, setLoop] = useState(false);
+  const [time, setTime] = useState(0);
 
   useEffect(() => {
-    currentMusic.audio.play();
+    // if (playing) currentMusic.audio.play();
     return () => {
-      currentMusic.audio.pause();
+      // currentMusic.audio.pause();
+      setTime(0);
     };
   }, [currentMusic]);
 
   useEffect(() => {
-    if (!playing) {
-      currentMusic.audio.pause();
-    } else {
+    if (playing) {
       currentMusic.audio.play();
     }
-  }, [playing]);
+    const timeFlow = playing
+      ? setInterval(() => {
+          if (currentMusic.audio.ended) setPlaying(false);
+          setTime(Math.round(currentMusic.audio.currentTime));
+        }, 1000)
+      : null;
+    return () => {
+      currentMusic.audio.pause();
+      if (timeFlow) clearInterval(timeFlow);
+    };
+  }, [currentMusic, playing]);
+
+  useEffect(() => {
+    currentMusic.audio.loop = loop;
+  }, [loop]);
 
   return (
     <div className="musicControllerContainer">
@@ -65,6 +81,56 @@ const MusicController = () => {
         ) : (
           <PlayImg onClick={() => setPlaying(true)} className="playPauseBtn" />
         )}
+        <div className="playOptionContainer">
+          <RepeatImg
+            fill={loop ? "black" : "gray"}
+            onClick={() => {
+              setLoop((loop) => !loop);
+            }}
+          />
+        </div>
+        <div
+          style={{
+            width: "100%",
+            height: "10px",
+            border: "1px solid",
+            background: "gray",
+            position: "relative",
+          }}
+          onClick={(e) => {
+            const timePoint =
+              ((e.clientX - e.currentTarget.offsetLeft) /
+                e.currentTarget.offsetWidth) *
+              currentMusic.audio.duration;
+            currentMusic.audio.currentTime = timePoint;
+            setTime(timePoint);
+          }}
+        >
+          <div
+            style={{
+              width: `${(time / currentMusic.audio.duration) * 100}%`,
+              height: "100%",
+              background: "black",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "10px",
+              height: "10px",
+              background: "white",
+              position: "absolute",
+              top: 0,
+              left: `${(time / currentMusic.audio.duration) * 100 - 2}%`,
+              border: "1px solid",
+              borderRadius: "50%",
+            }}
+          ></div>
+        </div>
+        <span>
+          {("0" + Math.floor(time / 60).toString()).slice(-2) +
+            ":" +
+            ("0" + Math.floor(time % 60).toString()).slice(-2)}
+        </span>
       </div>
     </div>
   );
