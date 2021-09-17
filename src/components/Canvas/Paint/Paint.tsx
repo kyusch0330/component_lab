aaef88b2236b3a41480404e68c5ea07f686ed6a3
kschoi <kyusch0330@gmail.com>
2021-09-18 03:42:05 +0900

    [Feat] Paint 그리기, 배경색 채우기 기능 추가

--- src/components/Canvas/Paint/Paint.tsx
+++ src/components/Canvas/Paint/Paint.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Paint.scss";
 import { ReactComponent as BrushImg } from "assets/brush.svg";
 import { ReactComponent as FillImg } from "assets/fill.svg";
@@ -15,29 +15,117 @@ const Paint = () => {
     { colorName: "green", colorValue: "#4ece4e" },
     { colorName: "blue", colorValue: "#4e4ece" },
     { colorName: "purple", colorValue: "#ae4ede" },
+    { colorName: "pink", colorValue: "#fe9e9e" },
+    { colorName: "darkred", colorValue: "#8e2e2e" },
+    { colorName: "brown", colorValue: "#8e5e1e" },
+    { colorName: "darkgreen", colorValue: "#2e6e2e" },
+    { colorName: "cyan", colorValue: "#1eaeae" },
+    { colorName: "skyblue", colorValue: "#6eaefe" },
+    { colorName: "navy", colorValue: "#1e1e6e" },
   ];
-  const brushSizes = [1, 2, 3, 4, 5];
+  const brushSizes = [1, 2, 3, 4, 5, 6];
 
   const [paintMode, setPaintMode] = useState(0);
   // 0: brush, 1: fill
   const [color, setColor] = useState(colors[0]);
   const [brushSize, setBrushSize] = useState(1);
+  const [painting, setPainting] = useState(false);
+
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D>();
+
+  useEffect(() => {
+    const ctxInit = canvas.current?.getContext("2d");
+    if (!ctxInit) return;
+    ctxInit.strokeStyle = colors[0].colorValue;
+    ctxInit.fillStyle = colors[0].colorValue;
+    ctxInit.lineWidth = brushSizes[0];
+
+    setCtx(ctxInit);
+  }, []);
+
+  useEffect(() => {
+    if (!ctx) return;
+    ctx.strokeStyle = color.colorValue;
+    ctx.fillStyle = color.colorValue;
+  }, [color]);
+
+  useEffect(() => {
+    if (!ctx) return;
+    ctx.lineWidth = brushSize;
+  }, [brushSize]);
+
+  const handleBrushMove = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+  ) => {
+    const x = e.nativeEvent.offsetX;
+    const y = e.nativeEvent.offsetY;
+    if (!painting) {
+      ctx?.beginPath(); //경로 생성
+      ctx?.moveTo(x, y); //선 시작 좌표
+    } else {
+      ctx?.lineTo(x, y); //선 끝 좌표
+      ctx?.stroke(); //선 그리기
+    }
+  };
+
+  const handleBrushTouchStart = (e: React.TouchEvent<HTMLCanvasElement>) => {
+    const x = e.touches[0].clientX - Number(canvas.current?.offsetLeft);
+    const y =
+      e.touches[0].pageY -
+      Number(canvas.current?.offsetTop) +
+      document.getElementsByClassName("paintContainer")[0].scrollTop;
+    ctx?.beginPath(); //경로 생성
+    ctx?.moveTo(x, y); //선 시작 좌표
+  };
+  const handleBrushTouchMove = (e: React.TouchEvent<HTMLCanvasElement>) => {
+    const x = e.touches[0].clientX - Number(canvas.current?.offsetLeft);
+    const y =
+      e.touches[0].pageY -
+      Number(canvas.current?.offsetTop) +
+      document.getElementsByClassName("paintContainer")[0].scrollTop;
+    ctx?.lineTo(x, y); //선 끝 좌표
+    ctx?.stroke(); //선 그리기
+  };
 
   return (
-    <div className="paintContainer">
-      <canvas id="jsPaintCanvas" className="paintCanvas"></canvas>
+    <div
+      className="paintContainer"
+      style={{ overflow: painting ? "hidden" : "scroll" }}
+    >
+      <canvas
+        id="jsPaintCanvas"
+        ref={canvas}
+        className="paintCanvas"
+        width={300} //css와 별개로 크기를 지정해줘야 함
+        height={400}
+        onMouseMove={(e) => paintMode === 0 && handleBrushMove(e)}
+        onMouseDown={(e) => paintMode === 0 && setPainting(true)}
+        onMouseUp={(e) => paintMode === 0 && setPainting(false)}
+        onMouseLeave={(e) => paintMode === 0 && setPainting(false)}
+        onTouchStart={(e) => paintMode === 0 && handleBrushTouchStart(e)}
+        onTouchMove={(e) => {
+          paintMode === 0 && handleBrushTouchMove(e);
+        }}
+        onTouchEnd={(e) => paintMode === 0 && setPainting(false)}
+        onClick={(e) => {
+          if (paintMode === 1) {
+            ctx?.fillRect(0, 0, 300, 400);
+          }
+        }}
+      ></canvas>
       <div className="controls">
         <div className="controls__brushSizes">
           <input
             type="range"
             min="1"
-            max="5"
+            max={brushSizes[brushSizes.length - 1]}
             step="1"
             value={brushSize}
             onChange={(e) => setBrushSize(Number(e.target.value))}
           />
           {brushSizes.map((size) => {
-            const viewSize = `${size * 5 + 5}px`;
+            const viewSize = `${size * 2 + 10}px`;
             return (
               <div
                 className={`controls__brushSize${
@@ -47,6 +135,7 @@ const Paint = () => {
                   width: viewSize,
                   height: viewSize,
                 }}
+                onClick={(e) => setBrushSize(size)}
               ></div>
             );
           })}
