4cd0f2b058f217ba2e8923a3b21abd7a29c8844a
kschoi <kyusch0330@gmail.com>
2022-01-07 22:47:09 +0900

    [Feat] 선택된 PageMenu Item 표시 수정

--- src/views/PageMenu/PageMenu.tsx
+++ src/views/PageMenu/PageMenu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ReactComponent as ArrowImg } from "assets/right-arrow.svg";
 import "./PageMenu.scss";
 
@@ -14,9 +14,10 @@ const PageMenu = ({
   display,
   setDisplay,
   paths,
-  current,
+  current, //더 이상 쓰이지 않음
   setCurrent,
 }: Props) => {
+  const location = useLocation();
   return (
     <div className={`pageMenuContainer ${!display && "closed"}`}>
       <div className="sliderMenu">
@@ -24,7 +25,12 @@ const PageMenu = ({
           <Link
             key={pIndex}
             onClick={() => setCurrent(pIndex)}
-            className={`menu ${current === pIndex && "selected"}`}
+            className={`menu ${
+              location.pathname.split("/")[
+                location.pathname.split("/").length - 1
+              ] === path[1].split("/")[path[1].split("/").length - 1] &&
+              "selected"
+            }`}
             to={path[1]}
           >
             {path[0]}
